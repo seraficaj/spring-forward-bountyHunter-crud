ebdac9a093cd31cced50f3e133bb2e1f20333fcc
Jason Serafica <seraficaj@gmail.com>
2022-03-17 14:07:59 -0700

    add get 1 bounty route

--- controllers/bounty.js
+++ controllers/bounty.js
@@ -1,22 +1,46 @@
-const db = require('../models');
+const db = require("../models");
 const router = require("express").Router();
 
 // define routes here
 
 // GET /bounties (index)
+router.get("/", async (req, res) => {
+    try {
+        // find all bounties
+        const bounties = await db.Bounty.find({});
+        // respond with all the bounties
+        res.json(bounties);
+        // if there is an error, we will send an error status w/ message
+    } catch (err) {
+        console.log("Error!", err);
+        res.status(503).json({ message: "DB/Server Error!" });
+    }
+});
 
 // GET /bounties/:id
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+
+    db.Bounty.findById(id).then((foundBounty) => {
+        if (!foundBounty) {
+            return res.status(404).json({ message: "DB/Server Error!" });
+        }
+        res.json(foundBounty);
+    }).catch(err => {
+        res.status(503).json({ message: "DB/Server Error!" });
+    });
+});
 
 // POST /bounties (insert a new bounty)
-router.post('/', async (req,res) => {
+router.post("/", async (req, res) => {
     try {
         const createdBounty = await db.Bounty.create(req.body);
         res.status(201).json(createdBounty);
     } catch (err) {
-        if (err.name === 'ValidationError'){
-            res.status(406).json({message: 'Validation Error!'});
+        if (err.name === "ValidationError") {
+            res.status(406).json({ message: "Validation Error!" });
         } else {
-            res.status(503).json({message: 'DB/Server Error!'})
+            res.status(503).json({ message: "DB/Server Error!" });
         }
     }
 });
@@ -25,8 +49,8 @@ router.post('/', async (req,res) => {
 
 // DELETE /bounties/:id (destroy)
 
-router.get('/', (req,res) => {
-    res.json({message: 'hello from bounty controller'})
+router.get("/", (req, res) => {
+    res.json({ message: "hello from bounty controller" });
 });
 
 module.exports = router;
